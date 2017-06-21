d4b2e7a01b1f020acc78aeceb57a134ca829b6b2
Romka <elberg2002@gmail.com>
2017-06-21 23:23:29 +0300

    add changes

---
+++ check.py
@@ -0,0 +1,5 @@
+def check(el): # Проверка, работает нестабильно
+    if el:
+        print("| CONNECTION ESTABILISHED!" + "\n")
+    else:
+        print("ERROR!")
---
+++ weather.py
@@ -0,0 +1,5 @@
+from geopy.geocoders import GoogleV3
+import requests
+import json
+
+
